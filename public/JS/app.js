3e6957ecdf4615f7f50733a361f81e46c4ff5985
Peter Calabrese <peter@calabrese.tech>
2021-06-30 22:31:12 -0400

    removed binaries

--- public/JS/app.js
+++ public/JS/app.js
@@ -1,6 +1,35 @@
 const contactForm = document.querySelector('[contact-form]')
+let uName = document.getElementById('name')
+let email = document.getElementById('email')
+let subject = document.getElementById('subject')
+let message = document.getElementById('message')
 
 contactForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log('Submit Clicked')
+
+    let formData = {
+        name: uName.value,
+        email: email.value,
+        subject: subject.value,
+        message: message.value
+    }
+
+    let xhr = new XMLHttpRequest()
+    xhr.open('POST', 'https://127.168.1.1:5000')
+    xhr.setRequestHeader('content-type', 'application/json')
+    xhr.onload = function(){
+        console.log(xhr.responseText)
+        if(xhr.responseText=='success'){
+            alert('Email Sent')
+            uName.value=''
+            email.value=''
+            subject.value=''
+            message.value=''
+            
+        }else{
+            alert('Something went wrong')
+        }
+    }
+
+    xhr.send(JSON.stringify(formData))
 })
